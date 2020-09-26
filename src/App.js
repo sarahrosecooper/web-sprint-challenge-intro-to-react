f6ffaf17d49ac27a74ea99e8d911aee6b0e46044
sarah rose <xsarahrosemeow@gmail.com>
2020-09-25 21:51:44 -0400

    data is displaying in console properly

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then((response) => {
+        return setCharacters(response.data.results);
+      })
+      .catch((error) => console.log("Uh-oh, we have an error!", error));
+  }, []);
+
+  console.log(characters);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
